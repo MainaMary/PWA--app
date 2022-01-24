f91754b7f5d2bed8b333f65f493e11d0d3ed8146
MainaMary <marieshix98@gmail.com>
2022-01-24 11:18:26 +0300

    use of ternary operator for conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
-import {addUser, deleteUser, updateUser} from "./redux/Users"
+import { addUser, deleteUser, updateUser } from "./redux/Users";
 
 function App() {
-  const dispatchAction= useDispatch()
+  const dispatchAction = useDispatch();
   const usersList = useSelector((state) => state.users.value);
-  
-  const [name, setName]= useState("")
-  const [email, setEmail] = useState("")
-  
-  const handleName= (e)=>{
-  setName(e.target.value)
-  }
-  const handleEmail =(e)=>{
-    setEmail(e.target.value)
-  }
 
-  
-  const newId=  usersList.length && usersList[usersList.length -1].id + 1
-  
-   
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [newuserName, setNewusername] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleNewUserName = (e) => {
+    setNewusername(e.target.value);
+  };
+
+  const newId = usersList.length && usersList[usersList.length - 1].id + 1;
+
   return (
     <div className="App">
       <div>
-        <form className="add-user" onSubmit={(e)=>{e.preventDefault() 
-        dispatchAction(addUser({id:newId , name:name, email: email}))
-        }}>
-          <input type="text" palceholder="Name"  onChange={handleName}/>
+        <form
+          className="add-user"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatchAction(addUser({ id: newId, name: name, email: email }));
+          }}
+        >
+          <input type="text" palceholder="Name" onChange={handleName} />
           <input type="text" placeholder="Email" onChange={handleEmail} />
-          <button >Add user</button>
+          <button>Add user</button>
         </form>
         <div className="display-users">
           {usersList.map((user) => {
@@ -37,22 +44,32 @@ function App() {
               <div key={user.id}>
                 <p>{user.name}</p>
                 <p>{user.email}</p>
-                <input type="text" />
-               
-                <i 
-                onClick={()=>{
-                  dispatchAction(updateUser({
-                    id: user.id,
-                    name: name
-                  }))
-                }}
-                className="fas fa-user-edit"></i>
+                <input
+                  type="text"
+                  placeholder="new name..."
+                  onChange={handleNewUserName}
+                />
 
+                <i
+                  onClick={() => {
+                    console.log(user.id);
+                    dispatchAction(
+                      updateUser({
+                        id: user.id,
 
-                <i onClick= {()=>{
-                  dispatchAction(deleteUser({id:user.id}))
-                }}className="fas fa-trash-alt"></i>
-               
+                        name: newuserName,
+                      })
+                    );
+                  }}
+                  className="fas fa-user-edit"
+                ></i>
+
+                <i
+                  onClick={() => {
+                    dispatchAction(deleteUser({ id: user.id }));
+                  }}
+                  className="fas fa-trash-alt"
+                ></i>
               </div>
             );
           })}
--- src/redux/Users.js
+++ src/redux/Users.js
@@ -1,29 +1,32 @@
-import {createSlice} from "@reduxjs/toolkit"
-import {userData} from "./UsersData"
+import { createSlice } from "@reduxjs/toolkit";
+import { userData } from "./UsersData";
 export const userSlice = createSlice({
-    name: "users",
-    initialState:{value: userData},
-    reducers:{
-        addUser: (state, action) =>{
-            //add a user to the state
-            state.value.push(
-                action.payload
-            )
-        },
-        //delete user
-        deleteUser : (state, action)=>{
-            state.value=state.value.filter(user=>user.id !==action.payload.id)
-        
-        },
-        
-        updateUser: (state, action)=>{
-         state.value.map((user)=>{
-             if(user.id === action.payload.id){
-                 user.name= action.payload.name
-             }
-         })
+  name: "users",
+  initialState: { value: userData },
+  reducers: {
+    addUser: (state, action) => {
+      //add a user to the state
+      state.value.push(action.payload);
+    },
+    //delete user
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.id !== action.payload.id);
+    },
+
+
+    updateUser: (state, action) =>{
+        state.value.forEach((user)=>{
+            console.log(`user updated ${user.id}  ${action.payload.name}`)
+            
+            const newUser =  user.name = action.payload.name
+            const newName = user.id === action.payload.id ? newUser : null
+
+            return newName
         }
+        )
     }
-})
-export const {addUser, deleteUser, updateUser}= userSlice.actions
+   
+  },
+});
+export const { addUser, deleteUser, updateUser } = userSlice.actions;
 export default userSlice.reducer;
